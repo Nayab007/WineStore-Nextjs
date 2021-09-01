9c614d79253c9dc2f447b45071079a148a7cd5f1
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-09-01 12:45:04 +0200

    first commit

---
+++ utils/styles.js
@@ -0,0 +1,106 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  navbar: {
+    backgroundColor: '#c2e9fb',
+    '& a': {
+      color: '#596164',
+      marginLeft: 10,
+    },
+  },
+  brand: {
+    fontWeight: 'bold',
+    fontSize: '1.5rem',
+  },
+  grow: {
+    flexGrow: 2,
+  },
+  main: {
+    minHeight: '80vh',
+  },
+  footer: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  form: {
+    width: '100%',
+    maxWidth: 800,
+    margin: '0 auto',
+  },
+  navbarButton: {
+    color: '#243949',
+    textTransform: 'initial',
+  },
+  transparentBackground: {
+    backgroundColor: 'transparent',
+  },
+  error: {
+    color: '#f04040',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  reviewForm: {
+    maxWidth: 800,
+    width: '100%',
+  },
+  reviewItem: {
+    marginRight: '1rem',
+    borderRight: '1px #808080 solid',
+    paddingRight: '1rem',
+  },
+  toolbar: {
+    justifyContent: 'space-between',
+  },
+  menuButton: { padding: 0 },
+  mt1: { marginTop: '1rem' },
+  // search
+  searchSection: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  searchForm: {
+    border: '1px solid #ffffff',
+    backgroundColor: '#ffffff',
+    borderRadius: 5,
+  },
+  searchInput: {
+    paddingLeft: 5,
+    color: '#000000',
+    '& ::placeholder': {
+      color: '#606060',
+    },
+  },
+  iconButton: {
+    backgroundColor: '#a8f3f8 ',
+    padding: 5,
+    borderRadius: '0 5px 5px 0',
+    '& span': {
+      color: '#1e3c72',
+    },
+  },
+  sort: {
+    marginRight: 5,
+  },
+
+  fullContainer: { height: '100vh' },
+  mapInputBox: {
+    position: 'absolute',
+    display: 'flex',
+    left: 0,
+    right: 0,
+    margin: '10px auto',
+    width: 300,
+    height: 40,
+    '& input': {
+      width: 250,
+    },
+  },
+}));
+export default useStyles;
