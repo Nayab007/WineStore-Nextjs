5f9b1c2b33a8e024d46843f59e29349cfb601cc6
Nayab Lone <55736152+Nayab007@users.noreply.github.com>
2021-09-03 17:27:21 +0200

    change in config file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  reactStrictMode: true,
-  images: { domains: ['res.cloudinary.com'] },
-};
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+}
